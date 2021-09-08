b1e5d83289bd74c3bea319b0a2cc6d1e2a5535e0
manfred-commits <manfredipiraino@gmail.com>
2021-09-08 11:24:07 +0200

    aggiunto array di oggetti

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,3 +1,56 @@
 // Crea un array di 10 oggetti che rappresentano una zucchina, indicando
 // per ognuna varietà, peso e lunghezza.
 // Calcola quanto pesano tutte le zucchine.
+
+var zucchina = [
+    {   
+        "varietà": "varietà-1",
+        "peso": 500,
+        "lunghezza": 30,
+    },
+    {   
+        "varietà": "varietà-2",
+        "peso": 600,
+        "lunghezza": 20
+    },
+    {   
+        "varietà": "varietà-3",
+        "peso": 20,
+        "lunghezza": 10
+    },
+    {   
+        "varietà": "varietà-4",
+        "peso": 100,
+        "lunghezza": 5
+    },
+    {   
+        "varietà": "varietà-5",
+        "peso": 700,
+        "lunghezza": 20
+    },
+    {   
+        "varietà": "varietà-6",
+        "peso": 800,
+        "lunghezza": 40
+    },
+    {   
+        "varietà": "varietà-7",
+        "peso": 200,
+        "lunghezza": 30
+    },
+    {   
+        "varietà": "varietà-8",
+        "peso": 600,
+        "lunghezza": 40
+    },
+    {   
+        "varietà": "varietà-9",
+        "peso": 700,
+        "lunghezza": 20
+    },
+    {   
+        "varietà": "varietà-10",
+        "peso": 20,
+        "lunghezza": 10
+    }
+]
