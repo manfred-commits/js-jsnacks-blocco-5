09d87b605e823e8117fe3a4b6b2a13d9b9a3f1b8
manfred-commits <manfredipiraino@gmail.com>
2021-09-08 17:06:30 +0200

    aggiunta validazione lunghezza argomenti

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -6,18 +6,60 @@
 function alternatArray(array1, array2){
 
    let letENumeri=[];
+   let newArr1=[];
+   let newArr2=[];
 
-    for(var i=0; i<array1.length;i++){
-        letENumeri.push(array1[i]);
-        letENumeri.push(array2[i]);
+
+   while(letENumeri==0){
+       if(array1.length==array2.length){
+           for(var i=0; i<array1.length;i++){
+
+               letENumeri.push(array1[i]);
+               letENumeri.push(array2[i]);
+               
+           }        
+       }else if(array1.length>array2.length){
+
+           
+        for(let i=0; i<array2.length;i++){
+            
+            newArr1.push(array1[i]);
+
+        }
+
+        array1=[];
+
+        for(let i=0; i<array2.length;i++){
+               
+            array1.push(newArr1[i]);
+
+        }
+
+       }else if(array2.length>array1.length){
         
+           
+        for(let i=0; i<array1.length;i++){
+            
+            newArr2.push(array2[i]);
+
+        }
+
+        array2=[];
+
+        for(let i=0; i<array1.length;i++){
+               
+            array2.push(newArr2[i]);
+
+        }
+       
     }
+   }
     return letENumeri;
 }
 
 
-var lettere=["a","b","c"];
-var numeri=[1,2,3];
+var lettere=["a","b","c",];
+var numeri=[1,2,3,4];
 
 
 
