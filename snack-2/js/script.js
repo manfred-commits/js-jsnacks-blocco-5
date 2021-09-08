006f63135d9ec9faf22b8d5735c798fe0328a88f
manfred-commits <manfredipiraino@gmail.com>
2021-09-08 11:46:03 +0200

    rinominato snack e aggiunto base

---
+++ snack-2/js/script.js
@@ -0,0 +1,60 @@
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+
+
+
+
+var zucchina = [
+    {   
+        "varietà": "varietà-1",
+        "peso": 500,
+        "lunghezza": 30,
+    },
+    {   
+        "varietà": "varietà-2",
+        "peso": 600,
+        "lunghezza": 20
+    },
+    {   
+        "varietà": "varietà-3",
+        "peso": 20,
+        "lunghezza": 10
+    },
+    {   
+        "varietà": "varietà-4",
+        "peso": 100,
+        "lunghezza": 5
+    },
+    {   
+        "varietà": "varietà-5",
+        "peso": 700,
+        "lunghezza": 20
+    },
+    {   
+        "varietà": "varietà-6",
+        "peso": 800,
+        "lunghezza": 40
+    },
+    {   
+        "varietà": "varietà-7",
+        "peso": 200,
+        "lunghezza": 30
+    },
+    {   
+        "varietà": "varietà-8",
+        "peso": 600,
+        "lunghezza": 40
+    },
+    {   
+        "varietà": "varietà-9",
+        "peso": 700,
+        "lunghezza": 20
+    },
+    {   
+        "varietà": "varietà-10",
+        "peso": 20,
+        "lunghezza": 10
+    }
+]
