56292c1348538fa22d19412b103c05cb895d0fcd
manfred-commits <manfredipiraino@gmail.com>
2021-09-08 12:58:41 +0200

    aggiunto soluzione base in funzione

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -3,10 +3,21 @@
 //     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
 
+function alternatArray(array1, array2){
+
+   let letENumeri=[];
+
+    for(var i=0; i<array1.length;i++){
+        letENumeri.push(array1[i]);
+        letENumeri.push(array2[i]);
+        
+    }
+    return letENumeri;
+}
+
 
 var lettere=["a","b","c"];
 var numeri=[1,2,3];
-var letENumeri=[];
 
 
 
@@ -14,10 +25,6 @@ console.log(lettere);
 
 console.log(numeri);
 
-for(var i=0; i<lettere.length;i++){
-    letENumeri.push(lettere[i]);
-    letENumeri.push(numeri[i]);
-    
-}
 
-console.log(letENumeri);
+
+console.log("L'array ricavato è:",(alternatArray(lettere, numeri)));
